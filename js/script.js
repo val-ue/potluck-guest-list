95e4d15c7f0dcfde570e3ce5271d1d3547c3ba40
valerie_l <contacttovalerie@gmail.com>
2023-04-05 16:52:59 -0400

    Guest list addition

--- js/script.js
+++ js/script.js
@@ -10,3 +10,34 @@ const guestList = document.querySelector(".guest-list");
 const guestCount = document.querySelector(".attendance");
 // alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
+
+addGuestButton.addEventListener("click", function () {
+    const guest = guestInput.value;
+    if (guest !== "") {
+      addGuest(guest);
+      clearInput();
+      updateGuestCount();
+    }
+  });
+  
+const addGuest = function (guest) {
+    const listItem = document.createElement("li");
+    listItem.innerText = guest;
+    guestList.append(listItem);
+};
+  
+const clearInput = function () {
+    guestInput.value = "";
+};
+  
+const updateGuestCount = function () {
+    const guests = document.querySelectorAll(".guest-list li");
+    guestCount.innerText = guests.length;
+  
+    if (guests.length === 8) {
+      addGuestButton.classList.add("hide");
+      guestInput.classList.add("hide");
+      guestInputLabel.classList.add("hide");
+      guestFull.classList.remove("hide");
+    }
+};
